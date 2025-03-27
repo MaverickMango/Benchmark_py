04087c0e9734e45e93530fa7305f4673f8c95da8
MaverickMango <1397959741@qq.com>
2025-03-27 00:53:07 -0700

    update scripts

--- tests_study/run_tests.py
+++ tests_study/run_tests.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 bugs_root = '/mnt/experiments/bugs'
 tests_root = '/mnt/experiments/APCA21/RGT/2019/evosuite'
-result_root = '/mnt/Benchmark_py/tests_study/buggy_result/'
+result_root = '/mnt/Benchmark_py/tests_study/'
 bugs_info = '/mnt/Benchmark_py/bugs_inputs.csv'
 
 defects4j_root = os.getenv('DEFECTS4J_HOME')
@@ -14,28 +14,9 @@ run_tests_cmd = '/'.join([defects4j_root, 'framework', 'bin', 'run_external_test
 cwd = os.getcwd()
 
 
-def checkout(proj, id, work_dir):
+def checkout(proj, id, work_dir, version):
     if os.path.exists(work_dir):
         return
-    print(f'checking out {proj}_{id}...')
-    checkout_cmd = f'defects4j checkout -p {proj} -v {id}b -w {work_dir}'
-    checkout_cmd = checkout_cmd.split(' ')
-    run_cmd(cwd, checkout_cmd)
-
-
-def run_tests(word_dir, proj, id, version, test_prefix, test_dir, log_file, failing_output):
-    tmp_dir = f'/mnt/tmp/{proj}_{id}'
-    if not os.path.exists(tmp_dir):
-        shutil.copytree(word_dir, tmp_dir)
-    test_dir = f'{tests_root}/{proj}/{id}/{test_dir}'
-    log_file = f'{result_root}/{test_prefix}/{proj}/{id}/{test_dir}/{log_file}'
-    if os.path.exists(log_file):
-        os.remove(log_file)
-    os.makedirs(os.path.dirname(log_file), exist_ok=True)
-    failing_output = f'{result_root}/{test_prefix}/{proj}/{id}/{test_dir}/{failing_output}'
-    if os.path.exists(failing_output):
-        os.remove(failing_output)
-    
     if version == 'original':
         version = 'o'
     elif version == 'inducing':
@@ -46,19 +27,55 @@ def run_tests(word_dir, proj, id, version, test_prefix, test_dir, log_file, fail
         version = 'f'
     else:
         version = 'b'
+    print(f'checking out {proj}_{id}{version}...')
+    checkout_cmd = f'defects4j checkout -p {proj} -v {id}{version} -w {work_dir}'
+    checkout_cmd = checkout_cmd.split(' ')
+    run_cmd(cwd, checkout_cmd)
+
+
+def run_tests(tmp_dir, proj, id, version, test_prefix, test_dir, include, log_file, failing_output):
+
+    log_file = f'{result_root}/{test_prefix}/{proj}/{id}/{test_dir}/{log_file}'
+    if os.path.exists(log_file):
+        os.remove(log_file)
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+    # print(log_file)
+    failing_output = f'{result_root}/{test_prefix}/{proj}/{id}/{test_dir}/{failing_output}'
+    if os.path.exists(failing_output):
+        os.remove(failing_output)
+    last_failing_test = f'{tmp_dir}/failing-tests.txt'
+    if os.path.exists(last_failing_test):
+        os.remove(last_failing_test)
     
-    cmd = f'{run_tests_cmd} -p {proj} -v {id}{version} -w {tmp_dir} -t {test_dir} -o {failing_output}'
-    print(f'running tests for {proj}_{id}_buggy...')
+    test_dir = f'{tests_root}/{proj}/{id}/{test_dir}'
+
+    if version == 'original':
+        version = 'o'
+    elif version == 'inducing':
+        version = 'i'
+    elif 'bug' in version:
+        version = 'b'
+    elif 'fix' in version:
+        version = 'f'
+    else:
+        version = 'b'
+
+    
+    print(f'running tests for {proj}_{id}{version}...')
+    cmd = f'{run_tests_cmd} -p {proj} -v {id}{version} -w {tmp_dir} -t {test_dir} -i {include}' # 加了-o之后没有junit执行的报错信息
+    print(cmd)
     cmd = cmd.split(' ')
-    result = run_cmd(word_dir, cmd)
-    
+    result = run_cmd(tmp_dir, cmd)
+    if os.path.exists(last_failing_test):
+        shutil.copyfile(last_failing_test, failing_output)
+
+    # print(result.stderr + result.stdout)
     if result.stdout == '0\n':
-        with open(log_file, 'x') as f:
-            f.write(result.stdout)
+        print('tests running success.')
     else:
         print('tests running failied.')
-        with open(log_file, 'x') as f:
-            f.write(result.stderr)
+    with open(log_file, 'x') as f:
+        f.write(result.stderr + result.stdout)
         
 
 
@@ -74,20 +91,36 @@ def run_cmd(work_dir, cmd):
 if __name__ == '__main__':
     log_file = 'logfile.txt'
     failing_output = 'failing_tests'
-    df = pd.read_csv(bugs_info)
-    proj_bugs = [item.split('_') for item in df['bug_name'].tolist()]
-    for proj_bug in proj_bugs:
-        proj = proj_bug[0]
-        id = proj_bug[1]
-        work_dir = f'{bugs_root}/{proj}/{proj}_{id}_buggy'
-        checkout(proj, id, work_dir)
-        test_dirs = [entry.name for entry in os.scandir(f'{tests_root}/{proj}/{id}') if entry.is_dir()]
-        for test_dir in test_dirs:
-            run_tests(work_dir, proj, id, 'buggy', 'evosuite2019', test_dir, log_file, failing_output)
+    test_prefix = 'evosuite2019'
+    if 'evosuite' in test_prefix:
+        include = '**/*_ESTest.java'
+    version = 'fixing'
+    result_root = f'{result_root}/{version}_result'
 
+    # df = pd.read_csv(bugs_info)
+    # proj_bugs = [item.split('_') for item in df['bug_name'].tolist()]
+    # for proj_bug in proj_bugs:
+    #     proj = proj_bug[0]
+    #     id = proj_bug[1]
+    #     work_dir = f'{bugs_root}/{proj}/{proj}_{id}_{version}'
+    #     test_dir_root = f'{tests_root}/{proj}/{id}'
+    #     if not os.path.exists(test_dir_root):
+    #         continue
+    #     checkout(proj, id, work_dir, version)
 
-    # proj = 'Lang'
-    # id = '51'
-    # test_dir = f'{tests_root}/{proj}/{id}/0'
-    # word_dir = f'{bugs_root}/{proj}/{proj}_{id}_buggy'
-    # run_tests(word_dir, proj, id, 'buggy', 'evosuite2019', test_dir, log_file, failing_output)
+    #     tmp_dir = f'/mnt/tmp/{proj}_{id}'
+    #     if os.path.exists(tmp_dir):
+    #         shutil.rmtree(tmp_dir)
+    #     shutil.copytree(work_dir, tmp_dir)
+
+    #     test_dirs = [entry.name for entry in os.scandir(test_dir_root) if entry.is_dir()]
+    #     for test_dir in test_dirs:
+    #         # print(test_dir)
+    #         run_tests(tmp_dir, proj, id, version, test_prefix, test_dir, include, log_file, failing_output)
+
+    proj = 'Math'
+    id = '50'
+    test_dir = f'1'
+    word_dir = f'{bugs_root}/{proj}/{proj}_{id}_{version}'
+    tmp_dir = f'/mnt/tmp/{proj}_{id}'
+    run_tests(tmp_dir, proj, id, version, test_prefix, test_dir, include, log_file, failing_output)
